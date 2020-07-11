49c619d4e9729514f592fcc054f9cce55a5c60d2
José Rodríguez García <joserodriguezgarcia95@gmail.com>
2020-07-11 12:51:53 +0200

    database script

---
+++ database/todo.sql
@@ -0,0 +1,13 @@
+IF (NOT EXISTS(SELECT *
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'dbo'
+    AND TABLE_NAME = 'todos'))
+    BEGIN
+    CREATE TABLE todos
+    (
+        [id] CHAR(36) NOT NULL,
+        [title] VARCHAR(255) NOT NULL,
+        [completed] BIT NOT NULL,
+        PRIMARY KEY ([id])
+    );
+END
